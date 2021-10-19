f525f1d65f348f34eda8e63aa791b3d8cf5dde59
Nobody <83214438+DanteDEVS@users.noreply.github.com>
2021-10-19 20:51:09 +0200

    pull it come on a update

--- src/Bavfalcon9/MultiVersion/protocol/ProtocolVersion.php
+++ src/Bavfalcon9/MultiVersion/protocol/ProtocolVersion.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Bavfalcon9\MultiVersion\protocol;
 
 class ProtocolVersion {
-    public const BETA_1_16_210_53 = 424;
-    public const BETA_1_16_210_50 = 423;
-    public const BETA_1_16_200_52 = 421;
-    public const v1_16_200 = 422; // current PM version
+    public const v1_17_40 = 471; # Current Pm Version
+    public const v1_17_30 = 465;
+    public const v1_17_10 = 448;
+    public const v1_17_0 = 440;
+    public const v1_16_220 = 431;
+    public const v1_16_200 = 422;
     public const v1_16_100 = 418;
     public const v1_16_20 = 408;
     public const v1_16_0 = 407;
@@ -17,14 +19,21 @@ class ProtocolVersion {
     public const v1_12_0 = 361;
 
     public const SUPPORTED_CLIENTS = [
-        self::v1_16_100,
-        self::v1_16_0,
-        self::v1_13_0,
-        self::v1_12_0
+    public const v1_17_30 = 465;
+    public const v1_17_10 = 448;
+    public const v1_17_0 = 440;
+    public const v1_16_220 = 431;
+    public const v1_16_200 = 422;
+    public const v1_16_100 = 418;
+    public const v1_16_20 = 408;
+    public const v1_16_0 = 407;
+    public const v1_14_60 = 390;
+    public const v1_13_0 = 388;
+    public const v1_12_0 = 361;
     ];
 
     public const SUPPORTED_SERVERS = [
-        self::v1_16_200
+        self::v1_17_20
     ];
 
     /**
